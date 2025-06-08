782459e7eabe076a29c9391606dae87a707ffefe
Aleksandra S <aleksandra.stupiec@gmail.com>
2025-06-08 01:19:10 -0600

    view circuito, reality composer changes

---
+++ Hackathon_swift_vision2/Circuito.swift
@@ -0,0 +1,18 @@
+//
+//  Circuito.swift
+//  Hackathon_swift_vision2
+//
+//  Created by Alumno on 07/06/25.
+//
+
+import SwiftUI
+
+struct Circuito: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Circuito()
+}
